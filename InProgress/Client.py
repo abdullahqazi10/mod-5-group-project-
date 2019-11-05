70c15eaff34044f83a1d5dfaaeadf2e5ec593ee8
Katona András <katonaandras@Katona-MacBook-Pro.local>
2019-11-05 17:21:45 +0100

    InProgress file with GUI and CLient for it

---
+++ InProgress/Client.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+from socket import AF_INET, socket, SOCK_STREAM, gethostname, gethostbyname
+from _thread import *
+import sys
+# import view.ViewTUI as ui
+import GUI
+
+TUI = 0
+HOST = ''
+PORT = 33000
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+SERVER=0
+
+"""
+handle messages starting with 'SYS'
+"""
+def server_output( msg):
+    GUI.server_msg(msg)
+
+"""
+handle messages starting with 'MSG'
+"""
+def client_output( msg):
+    GUI.receive(msg)
+
+"""
+listen to certain port for 1 connection
+"""
+def accept_con( ):
+    global SERVER
+    #allows for exactly one connection
+    SERVER = socket(AF_INET, SOCK_STREAM)
+    SERVER.bind(ADDR)
+    SERVER.listen(1)
+
+    server_output("listening")
+    while True:
+        client, client_address = SERVER.accept()
+        server_output("%s:%s has connected." % client_address)
+        client.send(bytes("SYSyou are connected to " + gethostname(), "utf8"))
+        handle_client(client)
+        break
+
+"""
+start reading from connection (different thread)
+  and reading from (cmd)input
+"""
+def handle_client(client):
+    #reads input
+    start_new_thread(receive, (client, ))
+    #listens to connection in parallel thread
+
+    readData(client)
+
+"""
+start listening to socket indefinitely, untill quit or empty
+"""
+def receive( socket):
+    while True:
+        try:
+            msg = socket.recv(BUFSIZ)
+            if str(msg)[:5] == "b'SYS" or str(msg) == "b''":
+                msg = msg.decode("utf8")
+                server_output(msg[3:])
+                if (msg ==  "" or msg == "SYS{quit}"):
+                    server_output("other disconnected, ^C to return to menu")
+                    disconnect(socket)
+                    break
+            elif msg.decode("utf8")[:3] == "MSG":
+                client_output(msg.decode("utf8")[3:])
+                pass
+            else:
+                #append_file(msg)
+                if (str(msg) != "b'{quit}'"):
+                    client_output(msg.decode("utf8"))
+                else:
+                    server_output("other disconnected")
+                    disconnect(socket)
+                    break
+        except OSError as e:  # Possibly other has left the chat.
+            server_output("receive(): disconnected error")
+            print(e)
+            break
+
+    sys.exit()
+
+"""
+send message on socket
+"""
+def send( msg, socket):
+    socket.send(bytes(msg, "utf8"))
+
+def disconnect( socket):
+    socket.close()
+
+"""
+connect to target IP, keeps going till connected
+"""
+def connect(target):
+    going = 1
+    while going:
+        HOST = ui.f_input('Enter server IP: ')
+        PORT = ui.f_input('Enter server port: ')
+        if not PORT:
+            PORT = 33000  # Default value.
+        else:
+            PORT = int(PORT)
+        BUFSIZ = 1024
+        ADDR = (HOST, PORT)
+        target = socket(AF_INET, SOCK_STREAM)
+        try:
+            target.connect(ADDR)
+        except OSError:
+            server_output("network not accepted, try another")
+            continue
+        try:
+            #start reading in seperate thread
+            start_new_thread( receive, (target, ))
+        except Exception as e:
+            print(e)
+        #start writing in this thread
+        going = 0
+        readData(target)
+
+"""
+let GUI connect
+"""
+def connect2(addr):
+    target = socket(AF_INET, SOCK_STREAM)
+    try:
+        target.connect(addr)
+    except OSError:
+        server_output("network not accepted, try another")
+        return -1
+    try:
+        #start reading in seperate thread
+        start_new_thread( receive, (target, ))
+    except Exception as e:
+        print(e)
+        return -1
+    readData(target)
+    start_new_thread(receive, (target, ))
+    return 1
+
+"""
+read imput from commandline
+"""
+CALLER = 0
+def readData(client):
+    global CALLER
+    CALLER = client
+    while TUI:
+        if data_input_handler("") == -1:
+            break
+    if TUI:
+        server_output("disconnected")
+
+"""
+send data from input to socket
+"""
+def data_input_handler( msg):
+    try:
+        if TUI:
+            msg = ui.f_input(">")
+        if msg == "{quit}":
+            server_output("disconnecting...")
+            send("SYS{quit}", CALLER)
+            disconnect(CALLER)
+            return -1
+        print("gona send it anyway")
+        send("MSG"+msg, CALLER)
+
+    except KeyboardInterrupt:
+        try:
+            send("SYS{quit}", CALLER)
+            disconnect(CALLER)
+        except:
+            pass
+        return -1
+    send(msg, CALLER)
+"""
+main thread, sets client to calling or recieving
+"""
+# while 1:
+#     try:
+#         choice = ui.f_input("what to do? 1=connect, 2=listen, 3=die")
+#         if choice == "1":
+#             target = 0
+#             connect(target)
+#         elif choice == "2":
+#
+#             # start_new_thread( accept_con, ())
+#             accept_con()
+#         elif choice == "3":
+#             sys.exit()
+#             break
+#     except KeyboardInterrupt:
+#         server_output("Shutting down")
+#         break
+#
+# """
+# do nothing until all threads are finished
+# """
+# while 1:
+#     pass
---
+++ InProgress/GUI.py
@@ -0,0 +1,116 @@
+import tkinter as tk
+from tkinter import *
+import socket
+import pdb
+import vlc
+import socket
+import time
+import subprocess
+import sys
+import Client as c
+
+
+
+HEIGHT = 800
+WIDTH = 800
+player=0
+root = tk.Tk()  # root window to place everything into
+port =-1
+ip=-1
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg='#00AFF0')
+canvas.pack()
+
+frame = tk.Frame(root, bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+portentry = tk.Entry(frame, font=40, text='Enter port number')
+portentry.place(relwidth=0.65, relheight=1)
+
+
+
+# instruction = tk.Text(frame, state=DISABLED)
+# instruction.place(relx=0.15, rely=0.01,relwidth=0.2, relheight=0.05)
+#
+# instruction.insert("end", "Enter port number\n")
+
+
+buttonc= tk.Button(root, text="Send", font=40, command= lambda: send())  # passing button to frame
+buttonc.place(x = 640,y = 606)
+
+
+
+message_frame = tk.Frame(root)
+message_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+textentry= tk.Entry(root, font=40, text='write message Here')
+textentry.place(relx=0.5, rely=0.85,relwidth=0.55, relheight=0.05, anchor='n')
+
+buttonl=tk.Button(frame, text="Listen", font=40, bg= 'yellow', command= lambda: listen())
+buttonl.pack(side=RIGHT)
+
+buttonc= tk.Button(frame, text="Connect", font=40, bg='green', command= lambda: sconnect()) # passing button to frame
+buttonc.pack(side=RIGHT)
+v = StringVar()
+
+textfield=tk.Label(message_frame, font=40,textvariable=v )
+
+
+textfield.place(relwidth=1, relheight=1)
+# textfield.insert("end","Enter port number in the connect field!\n")
+v.set(v.get()+"Enter port number in the connect field!\n")
+
+
+buttond=tk.Button(frame, text="Disconnect", font=40, bg= 'red', command= lambda: disconnectb())
+buttond.pack(side=RIGHT)
+
+def opendisplay():
+    cmdline = ['mplayer', '-fps', '25',  '1024', '-']
+    player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
+
+def receive(msg):
+    v.set(v.get()+msg+"\n")
+
+def send():
+    msg=textentry.get()
+    c.send(msg)
+
+def video(data):
+    player.stdin.write(data)
+
+
+def server_msg(msg):
+
+    v.set(v.get()+"Server:"+ msg +"\n")
+
+def sconnect():
+    global port, ip
+    input=portentry.get()
+    if(ip==-1):
+        ip=portentry.get()
+
+        v.set(v.get()+"Input the port address\n" )
+    else:
+        port=portentry.get()
+
+        v.set(v.get()+"Wait for instrcution from server\n" )
+
+        if(c.connect2((ip, port))!=-1):
+            pass
+        else:
+            port=-1
+            ip=-1
+
+
+
+def listen():
+    c.accept_con()
+
+def disconnectb():  #when disconnect ic clicked
+    port=0
+    ip=-1
+    print("Closing connection")
+    s.close()
+    print("Client disconnected")
+
+root.mainloop()  # run function
